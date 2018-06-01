d6cb0504492894f2368344f6c86b78d2098ffc3c
Yves Bovard <ybovard@gmail.com>
2018-06-01 21:50:33 +0200

    debug setup.py add tests directory

--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
 from setuptools import setup
 
 def readme():
-    with open('README.rst') as f:
+    with open('README.md') as f:
         return f.read()
 
 setup(name='gastonSyslogFW',
       version='1.0',
-      description=readme()
+      description=readme(),
       url='https://github.com/ybovard/gastonSyslogFW',
       author='Yves Bovard',
       author_email='ybovard@gmail.com',
       packages=['gastonSyslogFW'],
       install_requires=[ 'requests', ],
       include_package_data=True,
+      test_suite='nose.collector',
+      tests_require=['nose'],
       zip_safe=False)
