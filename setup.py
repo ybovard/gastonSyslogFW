4f6b719be6298368cfc7f086a2e68d814b746368
Yves Bovard <ybovard@gmail.com>
2018-06-01 17:38:32 +0200

    Add MANIFEST.in file change setup.py to include README.md

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup
 
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
 setup(name='gastonSyslogFW',
       version='1.0',
-      description='Syslog message forwarder',
+      description=readme()
       url='https://github.com/ybovard/gastonSyslogFW',
       author='Yves Bovard',
       author_email='ybovard@gmail.com',
       packages=['gastonSyslogFW'],
       install_requires=[ 'requests', ],
+      include_package_data=True,
       zip_safe=False)
